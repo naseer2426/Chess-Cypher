9396d91cc1f23de76c59bce777760d28513568f4
Naseer Ahmed Khan <naseer2426@Naseers-MacBook-Air.local>
2020-02-16 19:25:59 +0800

    Updates

---
+++ precache-manifest.17c61e6ef74c9fb5c8967cd49effed31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15957135e5a9a17ccf46402f4ba19993",
+    "url": "/chess-cypher/index.html"
+  },
+  {
+    "revision": "ce9560b391f8128808c7",
+    "url": "/chess-cypher/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "805212e74c37854bad09",
+    "url": "/chess-cypher/static/css/main.97b85bef.chunk.css"
+  },
+  {
+    "revision": "ce9560b391f8128808c7",
+    "url": "/chess-cypher/static/js/2.3452b7ff.chunk.js"
+  },
+  {
+    "revision": "063ab76dcf9610a4f22869b1e5220387",
+    "url": "/chess-cypher/static/js/2.3452b7ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "805212e74c37854bad09",
+    "url": "/chess-cypher/static/js/main.37b323ef.chunk.js"
+  },
+  {
+    "revision": "41dd14bfb8b269c5b4b4",
+    "url": "/chess-cypher/static/js/runtime-main.bf19b599.js"
+  }
+]);
